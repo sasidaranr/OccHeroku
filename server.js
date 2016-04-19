986921e147cc9754b5571004b7178b26218c0602
sasidaran <sasidaran.rajaram@listertechnologies.com>
2016-04-19 18:44:24 +0530

    logging the request

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/', function (req, res) {
 });
 
 app.post('/', function(req, res) {
-    console.log("request  ")
+    console.log("request---------------------------------  ",req)
       res.send('Hello World! POST');
 });
 
