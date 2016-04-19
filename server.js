88d79dc00f3dc646331a2b8d7d077391c3613c20
sasidaran <sasidaran.rajaram@listertechnologies.com>
2016-04-19 17:57:37 +0530

    basic express server

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
 var express = require('express');
-var https = require('https');
-var http = require('http');
-var fs = require('fs');
-
-// This line is from the Node.js HTTPS documentation.
-var options = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem')
-};
-
-// Create a service (the app object is just a callback).
 var app = express();
 
-// Create an HTTP service.
-http.createServer(app).listen(8000);
-// Create an HTTPS service identical to the HTTP service.
-//https.createServer(options, app).listen(443);
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
