69bcb3e708466048fae379dcf2ec4da0f13eaad4
sasidaran <sasidaran.rajaram@listertechnologies.com>
2016-04-19 18:16:20 +0530

    POST endpoint

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var app = express();
-var https = require('https');
-var fs = require('fs');
 
 app.set('port', (process.env.PORT || 5000));
 
-https.createServer({
-      key: fs.readFileSync('key.pem'),
-      cert: fs.readFileSync('cert.pem')
-    }, app).listen(app.get('port'));
+app.get('/', function (req, res) {
+  res.send('Hello World! GET');
+});
 
-    app.get('/', function (req, res) {
-      res.header('Content-type', 'text/html');
-      return res.end('<h1>Hello, Secure World!</h1>');
-    });
+app.post('/', function(req, res) {
+    console.log("request  ",req)
+      res.send('Hello World! POST');
+});
+
+app.listen(app.get('port'), function () {
+  console.log('Example app listening on port ', app.get('port'));
+});
